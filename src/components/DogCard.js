121f98ce12d383d3d11d4caa47fd8039d8696642
JackyLin2 <jacky.jia.lin@gmail.com>
2020-05-23 18:53:08 -0400

    Working on Nav

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DogCard = (props) => {
     return(
         <div>
-            <h1>HELLO</h1>
+            <h1>DOGG</h1>
         </div>
     )
 }
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,14 @@
+import React  from 'react'
+
+
+class MainContainer extends React.Component {
+    render(){
+        return(
+        <div>
+        </div>
+        )
+     
+    }
+}
+
+  export default MainContainer
