4a40eccd9a1e9eef6b171db52be0211e1bb43344
github username <github email address>
2020-05-25 16:49:47 -0400

    login working to save logged in dog, next setting up conditional rendering

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,41 +2,46 @@ import React from 'react';
 
 class Auth extends React.Component {
 
-    state = {
-        // username: '',
-        password: '',
-    }
-
-
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+    // handleChange = (event) => {
+    //     this.setState({
+    //         [event.target.name]: event.target.value
+    //     })
+    // }
 
     
 
     render(){
 
-        const {password} = this.state
-        const {setCurrentUser, username, handleUsername} = this.props
-        console.log(this.props)
-        return (
-        <form >
-            <h3>Sign In</h3>
-            <div>
-                <label>Username:</label>
-                <input onChange={handleUsername} type="text" name="username" value={username} placeholder="Enter Username" />
-            </div>
-            <div>
-                <label>Password</label>
-                <input onChange={this.handleChange} type="password" name="password" value={password}  placeholder="Enter password" />
-            </div>
-            <button onClick={() => setCurrentUser(username)}>Sign In</button>
-        </form>
-        )
+        // const {password} = this.state
+        const {setLoggedInDog, username, handleUsername, logged_in_dog, handleSignOut} = this.props
+
+        if (!logged_in_dog ) {
+            return (
+                <form >
+                    <h3>Sign In</h3>
+                    <div>
+                        <label>Username:</label>
+                        <input onChange={handleUsername} type="text" name="username" value={username} placeholder="Enter Username" />
+                    </div>
+                    {/* <div>
+                        <label>Password</label>
+                        <input onChange={this.handleChange} type="password" name="password" value={password}  placeholder="Enter password" />
+                    </div> */}
+                    <button onClick={(event) => setLoggedInDog(event, username)}>Sign In</button>
+                </form>
+                )
+        } else {
+            return (
+                <form >
+                    <h3>Sign Out</h3>
+                    <div>
+                        <p>Come back soon!</p>
+                    </div>
+                    <button onClick={(event) => handleSignOut(event)}>Sign Out</button>
+                </form>
+            )
+        }
     }
 }
 export default Auth;
 
-// onSubmit={this.handleSubmit}
