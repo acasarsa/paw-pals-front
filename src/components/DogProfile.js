cbaf0eefbe3c9a990f57100a8afef627014e1cbf
github username <github email address>
2020-05-26 11:55:12 -0400

    working on followers

--- src/components/DogProfile.js
+++ src/components/DogProfile.js
@@ -7,50 +7,126 @@ const url = 'http://localhost:3000/api/v1'
 class DogProfile extends React.Component {
 
     state = {
-        dog: null
+        dog: null,
+        followers: [],
     }
 
     componentDidMount() {
-        fetch(`${url}/dogs/${this.props.match.params.id}`)
-            .then(r => r.json())
-            .then(dog => this.setState({ dog: dog}), this.renderFollowers() )
+        this.getDogs()
     }
 
-    
-    // displayFollowerCount() => {
+    getDogs = () => {
+        fetch(`${url}/dogs/${this.props.match.params.id}`)
+            .then(r => r.json())
+            .then(dog => this.setState({ dog }), this.renderFollowers())
+    }
 
-    // }
+    renderFollowers = () => {
+        
+        if (this.state.dog) {
+            this.setState({followers: this.state.dog.followers})
+            this.state.followers.map(follower => <DogCard key={follower.id} {...follower}/>)
+        }
+
+    }
 
     // dog profile should be renamed to DogShowPage
 
-    renderFollowers = () => {
-        if (this.state.dog) {
-            this.state.dog.followers.map(follower => <DogCard key={follower.id} {...follower}/>)
+    handleFollow = (e) => {
+        e.preventDefault()
+        let newFollow = {
+            follower_id: this.props.loggedInDog.id,
+            followee_id: this.state.dog.id
         }
+    
+        let options = {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify(newFollow)
+        }
+    
+        fetch(`${url}/follows`, options)
+            .then(r => r.json())
+            .then( follower => {
+              this.setState({followers: [...this.state.followers, follower] //reset states })
+            })
+            // .then(r => r.json())
+            // .then(follower => this.setState({
+            //     follower
+            // }))
+        })
     }
+
+  
+
+
+    handleUnfollow = (id) => {
+        const options = {
+            method: 'DELETE'
+        }
+        fetch(`http://localhost:3000/endpoint/id`, options)
+            .then(r => r.json())
+            .then(this.setState({
+            followers: this.state.followers.filter((follower) => follower.id !== id)
+        }))
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+
+   
+    // need to get 
+
+    /// make it so you can't follow urself. and make it so the follow button 
+    // changes to a different form if u already follow them. 
+
+
+
+
 
     renderDogProfile = () => {
         if (this.state.dog) {
             console.log("followers off state on DP", this.state.dog.followers)
-        }
-    
-        const {name, image} = this.state.dog
-
-        return (
-            <>
-            <div>
-                <h2>Name: {name}</h2>
-                <img src={image}></img>
-                <hr></hr>
-                <h1>{name}'s Followers</h1>
-                <div className="simple-flex-row index-wrap">
-                {this.state.dog ?  this.state.dog.followers.map(follower => <DogCard key={follower.id} {...follower}/>) : "Be this pupperinos first follower!"}
-                </div>
         
-            </div>
-            </>
-        )
+    
+        const {name, image, followers} = this.state.dog
+
+        // if (!followers.includes(this.props.loggedInDog.id)) {
+                return (
+                    <>
+                    <div>
+                        <h2>Name: {name}</h2>
+                        <img src={image}></img>
+                        {!followers.includes(this.props.loggedInDog.id) ?
+                            <form onSubmit={this.handleFollow}>
+                                <button > Follow </button>
+                            </form>
+                        : 
+                            <form onSubmit={() => this.handleUnfollow()}>
+                                <button > Unfollow </button>
+                            </form>
+
+                        }
+                        
+                        <hr></hr>
+                        
+                        <h1>{name}'s Followers</h1>
+                        <div className="simple-flex-row index-wrap">
+                            {this.state.dog ?  this.state.dog.followers.map(follower => <DogCard key={follower.id} {...follower}/>) : "failed"}
+                        </div>
+                    </div>
+                    </>
+                )
+            }
+        // } else {
+            
+        // }
     }
+
+    // why does this work? but if i remove any of it it doesn't work anymore?? it seems like it's repeating itself ??
 
     /// you need to figure out how this will render. 3 container flex box probably 
     
