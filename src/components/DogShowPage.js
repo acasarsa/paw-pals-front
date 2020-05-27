abd2c0ca0f9ea08d77051c1440be244bc35ceb71
github username <github email address>
2020-05-27 19:02:49 -0400

    follows seem to be persiting

--- src/components/DogShowPage.js
+++ src/components/DogShowPage.js
@@ -10,38 +10,9 @@ class DogShowPage extends React.Component {
 
     componentDidMount() {
         this.renderDogShowPage()
-        // if (this.props.follow_id) {
 
-        //     console.log("follow_id", this.props.follow_id)
-        // }
     }
 
-    // getDogs = () => {
-    //     fetch(`${url}/dogs/${this.props.match.params.id}`)
-    //         .then(r => r.json())
-    //         .then(dog => this.setState({ dog }), this.renderFollowers())
-    // }
-
-    
-    // renderFollowers = () => {
-
-    //     if (this.state.dog) {
-    //         console.log("followers", this.state.dog.followers)
-    //         // console.log("followers")
-    //         this.state.dog.followers.map(follower => <DogCard key={follower.id} {...follower}/> )
-    //         // this.state.dog.followers.map(follower => <DogCard key={follower.id} {...follower}/>)
-    //         this.props.setFollowID(this.state.dog)
-    //     }
-
-    // }
-
-
-
-
-    
-
-
-    
     // need to get 
 
     /// make it so you can't follow urself. and make it so the follow button 
@@ -63,8 +34,8 @@ class DogShowPage extends React.Component {
             console.log('loggedinDOg', this.props.loggedInDog)
             // console.log('follow_id in renderDodpr', this.state.follow_id)
 
-        const {name, image, followers} = this.props.selected_dog
-        const {selected_dog} = this.props
+        const {name, image, followers, id} = this.props.selected_dog
+        const {selected_dog, handleFollow, handleUnfollow, loggedInDog} = this.props
         console.log("followers Show", followers);
 
 
@@ -73,18 +44,35 @@ class DogShowPage extends React.Component {
                 if (selected_dog) {
 
                     return (
+                        
                         <>
+                        { (loggedInDog.id ===  selected_dog.id) ? 
+                        
+                            <div>
+                            <h1>My Profile</h1>
+                            <h2>Name: {name}</h2>
+                            <img src={image}></img>
+                            
+                            <hr></hr>
+                            
+                            <h1>{name}'s Followers</h1>
+                            <div className="simple-flex-row index-wrap">
+                                {loggedInDog ?  selected_dog.followers.map(follower => <DogCard key={follower.id} {...follower}/>) : "failed"}
+                            </div>
+                        </div>
+
+                        :
                         <div>
                             <h2>Name: {name}</h2>
                             <img src={image}></img>
                             
-                            {followers.find((dog) => dog.id === this.props.loggedInDog.id) ?
+                            {followers.find((dog) => dog.id === loggedInDog.id) ?
                             <form>
-                                <button onClick={() => this.props.handleUnfollow(this.props.selected_dog)}> Unfollow </button>
+                                <button onClick={() => handleUnfollow(id)}> Unfollow </button>
                             </form>
                             
                             :
-                            <form onSubmit={(event) => this.props.handleFollow(event, this.props.selected_dog)}>
+                            <form onSubmit={(event) => handleFollow(event, selected_dog)}>
                                 <button > Follow </button>
                             </form>
                 
@@ -95,13 +83,16 @@ class DogShowPage extends React.Component {
                             
                             <h1>{name}'s Followers</h1>
                             <div className="simple-flex-row index-wrap">
-                                {this.props.selected_dog ?  this.props.selected_dog.followers.map(follower => <DogCard key={follower.id} {...follower}/>) : "failed"}
+                                {selected_dog ?  selected_dog.followers.map(follower => <DogCard key={follower.id} {...follower}/>) : "failed"}
                             </div>
                         </div>
+                        
+                        }
+                        
                         </>
                     )
                 } else {
-                   return (
+                    return (
                         <>
                             <h3>Go back and select a dog!</h3>
                             <button onClick={() => this.props.history.push('/dogs')}>Dog Index</button>
@@ -125,7 +116,7 @@ class DogShowPage extends React.Component {
 
         if (this.props) {console.log("is dog a prop?",this.props.dog)}
 
-        return  this.props ? this.renderDogShowPage() : <div> No Dog Selected... Try going back to Dogs! </div>
+        return  ( this.renderDogShowPage() )
 
         
     }
