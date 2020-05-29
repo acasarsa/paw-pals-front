ff0928649fb3b6f486f8678585bbc7866f14f86e
github username <github email address>
2020-05-28 20:00:03 -0400

    making new branch for pull test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import {Navbars} from 'react-bootstrap';
+
 
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
